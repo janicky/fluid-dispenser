0f177d2abeb9a3d790e4fdb5594a5b45f765e306
Patryk Janicki <kontakt.janicki@gmail.com>
2019-10-21 00:15:51 +0200

    Add comment headers

--- fluid-dispenser.ino
+++ fluid-dispenser.ino
@@ -2,6 +2,7 @@
 #include <ShiftRegister74HC595.h>
 #include <math.h>
 
+// =================== IN/OUT NUMBERS ==================
 #define TRIGGER_PIN  7
 #define ECHO_PIN     6
 #define MAX_DISTANCE 450
@@ -14,19 +15,23 @@
 #define MEASURE_DISTANCE_TASK 0
 #define TOGGLE_BUTTON_TASK 1
 
+// ===================== VARIABLES =====================
 // Multitasking millis variables
 unsigned long currentMillis = 0;
 unsigned long prevMillis[2];
 
+// ===================== INSTANCES =====================
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 ShiftRegister74HC595 sr(1, SHIFTREG_SERIAL_DATA_PIN, SHIFTREG_CLOCK_PIN, SHIFTREG_LATCH_PIN);
 
+// ======================= SETUP =======================
 void setup() {
   pinMode(4, INPUT);
   pinMode(5, OUTPUT);
   Serial.begin(9600);
 }
- 
+
+// ======================= LOOP ========================
 void loop() { 
   currentMillis = millis();
 
@@ -35,35 +40,39 @@ void loop() {
   }
 
   if (canPerformTask(TOGGLE_BUTTON_TASK, 100)) {
-    perfomToggleButtonTask();
+    if (digitalRead(4) == HIGH && !toggleButtonPressed) {
+      toggleButtonPressed = true;
+    } else if (digitalRead(4) == LOW && toggleButtonPressed) {
+      perfomToggleButtonTask();
+      toggleButtonPressed = false;
+    }
   }
-
-// Alternative for delay() function
-boolean canPerformTask(int index, unsigned long ms) {
-//  Check if task will should be performed
-  if (currentMillis - prevMillis[index] >= ms) {
-//    Update last perform time
-    prevMillis[index] = currentMillis;
-    return true;
-  }
-  return false;
-}
 
 // ======================= TASKS =======================
-// T01: Measure distance task
+// ----------------------- T01: Measure distance task
 void performMeasureDistanceTask() {
   int distance = sonar.ping_cm();
   Serial.println(distance);
 }
-// T02: Toggle button task
+
+// ----------------------- T02: Toggle button task
+boolean toggleButtonPressed = false;
 void perfomToggleButtonTask() {
-  if (digitalRead(4) == HIGH) {
-    Serial.println("Button pushed");
-//    bip();
-//    state = !state;
-//    while (digitalRead(4) == HIGH);
-  }
+
 }
+
+
+// ===================== FUNCTIONS =====================
+// Alternative for delay() function
+  boolean canPerformTask(int index, unsigned long ms) {
+// Check if task will should be performed
+    if (currentMillis - prevMillis[index] >= ms) {
+// Update last perform time
+      prevMillis[index] = currentMillis;
+      return true;
+    }
+    return false;
+  }
 
 void setFillingLevel(int level) {
   for (int i = 0; i < 8; i++) {
